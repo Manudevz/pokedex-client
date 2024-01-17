9deb2c3e6517b563570e5d00b2d600ad1dec7979
manudevz <enmanuelmarcanoservices@gmail.com>
2024-01-16 22:11:24 -0300

    x

--- src/context/PokemonsAppProvider.tsx
+++ src/context/PokemonsAppProvider.tsx
@@ -12,10 +12,14 @@ const PokemonProvider = ({ children }: PokemonProviderProps) => {
   const [isDataFetched, setIsDataFetched] = useState(false);
 
   useEffect(() => {
-    setChangePage(true);
+    if (isDataFetched) {
+      setChangePage(true);
+    }
+
     const source: CancelTokenSource = axios.CancelToken.source();
 
     const getPokemons = async (offset: number) => {
+
       try {
         const response = await axios.get<Pokemons[]>(
           `https://pokedex-server-y8gj.onrender.com/pokemons?offset=${offset}&limit=20`,
